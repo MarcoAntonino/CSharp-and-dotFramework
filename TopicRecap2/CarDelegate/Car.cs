64f0c0007a4f3d770fa74cea66851f1fcece4cdf
MarcoAntonino <marco.antonino@edu.itspiemonte.it>
2017-07-19 18:19:59 +0200

    Iniziati i delegate

---
+++ TopicRecap2/CarDelegate/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegate
+{
+    class Car
+    {
+        public const int MaxSpeed = 100;
+        public int CurrentSpeed { get; set; }
+        public string Petname { get; set; }
+
+
+        private bool carIsDead;
+
+        public Car()
+        {
+
+        }
+
+        public Car(string name, int currSp)
+        {
+            this.CurrentSpeed = currSp;
+            Petname = name; // a questo punto mi chiedo: quando si deve usare il this? 
+        }
+
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        private CarEngineHandler listOfHandlers;
+
+
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            this.listOfHandlers = methodToCall;
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                if (listOfHandlers != null)
+                {
+                    listOfHandlers("Sorry, this car is dead..."); // qui gli dico la stringa da passare al metodo. Ma Non abbiamo ancora un metodo
+                    //Infatti ecco perchè non capivo dove gli dicesse di stampare questo messaggio. Glielo dice nel programma dove viene scritto il metodo onCarEngineEvent. RIBADISCO: qui viene passata solo una stringa, quello che ci faremo con questa sitnrga, dipende dal metodo che verrà passato al "setter" RegisterWithCarEngine.
+                    //Perchè non è stato scritto qui il metodo onCarEngineEvent? Per fornire maggiore scalabilità. Nel senso che nella classe car noi predisponiamo che succeda qualcosa in determinate fasi (eventi); Nel metodo program, l'utente farà quello che vuole e in questo caso ha implementato il metodo onCarEngineEvent
+                }
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if (10 == (MaxSpeed - CurrentSpeed) && listOfHandlers != null)
+                {
+                    listOfHandlers("Careful buddy! gonna blow!");
+                }
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("CurretSpeed = {0}", CurrentSpeed);
+                }
+            }
+        }
+    }
+}
---
+++ TopicRecap2/CarDelegate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Delegates as event enablers");
+
+            Car c1 = new Car("SlugBug", 10);
+            c1.RegisterWithCarEngine(new Car.CarEngineHandler(onCarEngineEvent)); //qui gli passo il metodo che creo sotto. Occhio che è statico infatti non appartiene a nessuna variabile di car
+            //Inoltre RegisterWithCarEngine si aspetta un'istanza di CarEngineHandler che a sua volta contiene il meotod che verrà caricato in essa. Ecco perchè troviamo scritto new Car.CarEngineHandler(onCarEngineEvent)
+
+/*
+ * NOTA BENE:
+ * c1.RegisterWithCarEngine(onCarEngineEvent);
+ * Questa è un'alternativa a quello scritto sopra.
+ * C# permette di non istanziare un delegato CarEngineHandler che serve solo per passare il metodo onCarEngineEvent.
+ * Siccome il metodo ha la stessa signature, si può passare direttamente al "setter" RegisterWithCarEngine.
+ * Questo meccanismo si chiama Method Group Conversion
+ */
+
+Console.WriteLine("***** Speeding Up *****");
+for (int i = 0; i < 6; i++)
+{
+    c1.Accelerate(20);
+}
+Console.ReadLine();
+
+}
+
+public static void onCarEngineEvent(string msgForCaller) //ecco qui il metodo. Qui gli dico cosa fare con la stringa che gli ho passato nel metodo accelerate
+{
+Console.WriteLine("\n****Message from Car Object");
+Console.WriteLine("=> {0}", msgForCaller);            
+Console.WriteLine("**********************\n");
+}
+}
+}
